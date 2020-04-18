25b85887ee42f5f26c268d0b70047fbb70bdf8d7
BarneyPC <barnabas.tamas-nagy@gmx.at>
2020-04-18 17:19:34 +0200

    Grave

--- ludumdare46/Assets/Project/Scripts/PlayerController.cs
+++ ludumdare46/Assets/Project/Scripts/PlayerController.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float movmentSpeed = 10f;
+    private bool isGrave = false;
+    [SerializeField] private float interactTime = 2f;
+    private float downTime, upTime, pressTime = 0f;
+    private bool keyIsDown = false;
+    private bool graveIsOpen = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -26,12 +32,43 @@ public class PlayerController : MonoBehaviour
             transform.position += transform.right * horizontalMovment* movmentSpeed * Time.deltaTime;
         }
         
+        if (Input.GetKeyDown(KeyCode.E) && isGrave)
+        {
+            downTime = Time.time;
+            pressTime = downTime + interactTime;
+            keyIsDown = true;
+        }
+        if (Input.GetKeyUp(KeyCode.E) && isGrave)
+        {
+            if (Time.time < pressTime)
+            {
+                Debug.Log("interacted with grave");
+            }
+        }
+        if(Time.time >= pressTime && keyIsDown)
+        {
+            Debug.Log("dig up grave");
+            keyIsDown = false;
+        }
+
+
+
     }
     private void OnTriggerEnter2D(Collider2D collider)
     {
-        if (collider.gameObject.tag == "Grave")
+        if (collider.gameObject.CompareTag("Grave"))
         {
-
+            Debug.Log("grave");
+            isGrave = true;
         }
     }
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.gameObject.CompareTag("Grave"))
+        {
+            Debug.Log("grave left");
+            isGrave = false;
+        }
+    }
+
 }
