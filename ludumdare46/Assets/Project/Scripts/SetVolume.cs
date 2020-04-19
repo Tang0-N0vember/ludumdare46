6f77922f42180511fcb72a7832ea3f7aee9fffbb
Stefan Klimpfinger <stefan.klimpfinger@gmx.at>
2020-04-19 21:46:45 +0200

    AudioSlider + StartScreen

--- ludumdare46/Assets/Project/Scripts/SetVolume.cs
+++ ludumdare46/Assets/Project/Scripts/SetVolume.cs
@@ -1,18 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class SetVolume : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public AudioMixer mixer;
 
-    // Update is called once per frame
-    void Update()
+    public void SetLevel (float sliderValue)
     {
-        
+        mixer.SetFloat("MusicVol", Mathf.Log10(sliderValue) * 20);
     }
 }
