f01ab4b25c63a14a07f5b0ebe0d3f33e9f501718
Stefan Klimpfinger <stefan.klimpfinger@gmx.at>
2020-04-20 20:13:35 +0200

    Mondanimation

--- ludumdare46/Assets/Project/Scripts/ClockScript.cs
+++ ludumdare46/Assets/Project/Scripts/ClockScript.cs
@@ -7,9 +7,34 @@ public class ClockScript : MonoBehaviour
     [SerializeField] private GameObject mond;
     [SerializeField] private GameObject pivotPoint;
     [SerializeField] private GameObject logic;
-    
+    float interval;
+    float speed;
+
+    void Start()
+    {
+        speed = 5.0f;
+        StartCoroutine(RotateForSeconds());
+    }
+
     void Update()
     {
-        mond.transform.RotateAround(pivotPoint.transform.position, Vector3.forward ,-(/*(Mathf.PI*2)/*/logic.GetComponent<GameLogic>().getMaxGameTimer)/2 *Time.deltaTime);
+
+        //mond.transform.RotateAround(pivotPoint.transform.position, Vector3.forward ,-(/*(Mathf.PI*2)/*/logic.GetComponent<GameLogic>().getMaxGameTimer)/2 *Time.deltaTime);
+
+        //Debug.Log("MONDPOSITION" + (- (logic.GetComponent<GameLogic>().getMaxGameTimer) / 2 * Time.deltaTime));
+    }
+
+    IEnumerator RotateForSeconds() //Call this method with StartCoroutine(RotateForSeconds());
+    {
+        float time = logic.GetComponent<GameLogic>().getMaxGameTimer;     //How long will the object be rotated?
+
+        while (time > 0)     //While the time is more than zero...
+        {
+            mond.transform.RotateAround(pivotPoint.transform.position, Vector3.forward, -Time.deltaTime * speed);     //...rotate the object.
+            time -= Time.deltaTime;     //Decrease the time- value one unit per second.
+
+            yield return null;     //Loop the method.
+        }
+
     }
 }
