da117cd1bc67f4fbc5e8942e27faa2c1dc4cda64
BarneyPC <barnabas.tamas-nagy@gmx.at>
2020-04-20 12:58:39 +0200

    moster animation

--- ludumdare46/Assets/Project/Scripts/MonsterFollow.cs
+++ ludumdare46/Assets/Project/Scripts/MonsterFollow.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class MonsterFollow : MonoBehaviour
 {
+    [SerializeField] private GameObject monsterFront;
+    [SerializeField] private GameObject monsterBack;
+    [SerializeField] private GameObject monsterRight;
+    [SerializeField] private GameObject monsterRightAttack;
+    [SerializeField] private GameObject monsterRightHit;
+    [SerializeField] private GameObject monsterLeft;
+    [SerializeField] private GameObject monsterLeftAttack;
+    [SerializeField] private GameObject monsterLeftHit;
+
+
     [SerializeField] private float followSpeed;
 
     [SerializeField] private Transform player;
@@ -13,6 +23,18 @@ public class MonsterFollow : MonoBehaviour
     {
         player=GameObject.FindGameObjectWithTag("Player").transform;
         followSpeed = (float)gameObject.GetComponent<MonsterStats>().getCurrentMovement/50f;
+
+        monsterFront.SetActive(true);
+        monsterBack.SetActive(false);
+
+        monsterLeft.SetActive(false);
+        monsterLeftAttack.SetActive(false);
+        monsterLeftHit.SetActive(false);
+
+        monsterRight.SetActive(false);
+        monsterRightAttack.SetActive(false);
+        monsterRightHit.SetActive(false);
+
     }
 
     // Update is called once per frame
@@ -22,9 +44,113 @@ public class MonsterFollow : MonoBehaviour
         if (Vector2.Distance(transform.position, player.position) > 2f)
         {
             transform.position = Vector2.MoveTowards(transform.position, player.position, followSpeed * Time.deltaTime);
+
+
+            float xDistance = player.transform.position.x - gameObject.transform.position.x;
+            float yDistance = player.transform.position.y - gameObject.transform.position.y;
+            if (Mathf.Abs(xDistance) > Mathf.Abs(yDistance))
+            {
+                if (xDistance > 0)
+                {
+                    Debug.Log("East");
+                    monsterFront.SetActive(false);
+                    monsterBack.SetActive(false);
+                    monsterLeft.SetActive(false);
+                    monsterRight.SetActive(true);
+                }
+                else
+                {
+                    Debug.Log("West");
+                    monsterFront.SetActive(false);
+                    monsterBack.SetActive(false);
+                    monsterLeft.SetActive(true);
+                    monsterRight.SetActive(false);
+                }
+            }
+            else
+            {
+                if (yDistance > 0)
+                {
+                    Debug.Log("North");
+                    monsterFront.SetActive(false);
+                    monsterBack.SetActive(true);
+                    monsterLeft.SetActive(false);
+                    monsterRight.SetActive(false);
+                }
+                else
+                {
+                    Debug.Log("South");
+                    monsterFront.GetComponent<Animator>().SetBool("gehen", true);
+                    monsterFront.SetActive(true);
+                    monsterBack.SetActive(false);
+                    monsterLeft.SetActive(false);
+                    monsterRight.SetActive(false);
+                }
+            }
+        }
+        else
+        {
+           monsterFront.GetComponent<Animator>().SetBool("gehen", false);
+            monsterFront.SetActive(true);
+            monsterBack.SetActive(false);
+            monsterLeft.SetActive(false);
+            monsterRight.SetActive(false);
         }
     }
-    
+
+    public void attackRight()
+    {
+        monsterFront.SetActive(false);
+        monsterBack.SetActive(false);
+
+        monsterLeft.SetActive(false);
+        monsterLeftAttack.SetActive(false);
+        monsterLeftHit.SetActive(false);
+
+        monsterRight.SetActive(false);
+        monsterRightAttack.SetActive(true);
+        monsterRightHit.SetActive(false);
+    }
+    public void hitRight()
+    {
+        monsterFront.SetActive(false);
+        monsterBack.SetActive(false);
+
+        monsterLeft.SetActive(false);
+        monsterLeftAttack.SetActive(false);
+        monsterLeftHit.SetActive(false);
+
+        monsterRight.SetActive(false);
+        monsterRightAttack.SetActive(false);
+        monsterRightHit.SetActive(true);
+    }
+    public void attackLeft()
+    {
+        monsterFront.SetActive(false);
+        monsterBack.SetActive(false);
+
+        monsterLeft.SetActive(false);
+        monsterLeftAttack.SetActive(true);
+        monsterLeftHit.SetActive(false);
+
+        monsterRight.SetActive(false);
+        monsterRightAttack.SetActive(false);
+        monsterRightHit.SetActive(false);
+    }
+    public void hitLeft()
+    {
+        monsterFront.SetActive(false);
+        monsterBack.SetActive(false);
+
+        monsterLeft.SetActive(false);
+        monsterLeftAttack.SetActive(false);
+        monsterLeftHit.SetActive(true);
+
+        monsterRight.SetActive(false);
+        monsterRightAttack.SetActive(false);
+        monsterRightHit.SetActive(false);
+    }
+
 
 
 }
