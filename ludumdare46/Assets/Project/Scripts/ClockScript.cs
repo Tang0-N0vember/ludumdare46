168de3af7bef9b3c9636bf190362b3b18b0ad11f
BarneyPC <barnabas.tamas-nagy@gmx.at>
2020-04-20 15:12:55 +0200

    clock added

---
+++ ludumdare46/Assets/Project/Scripts/ClockScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClockScript : MonoBehaviour
+{
+    [SerializeField] private GameObject mond;
+    [SerializeField] private GameObject pivotPoint;
+    [SerializeField] private GameObject logic;
+    
+    void Update()
+    {
+        mond.transform.RotateAround(pivotPoint.transform.position, Vector3.forward ,-((Mathf.PI*2)/logic.GetComponent<GameLogic>().getMaxGameTimer)/2 *Time.deltaTime);
+    }
+}
---
+++ ludumdare46/Assets/Project/Scripts/TestClock.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestClock : MonoBehaviour
+{
+    [SerializeField] private float gameTimer;
+    private float currentGameTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentGameTimer = gameTimer;
+        if (currentGameTimer <= 0f)
+        {
+            Debug.Log("Game Over");
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        currentGameTimer -= Time.deltaTime;
+    }
+    public float getCurrentGameTimer
+    {
+        get { return currentGameTimer; }
+    }
+    public float getMaxGameTimer
+    {
+        get { return gameTimer; }
+    }
+}
