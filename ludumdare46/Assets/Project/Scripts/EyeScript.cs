2ad5019b338eb0efee3b386802c3ab53d70c16d0
BarneyPC <barnabas.tamas-nagy@gmx.at>
2020-04-20 16:26:37 +0200

    eye ui

---
+++ ludumdare46/Assets/Project/Scripts/EyeScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyeScript : MonoBehaviour
+{
+    [SerializeField] private GameObject eye1;
+    [SerializeField] private GameObject eye2;
+    [SerializeField] private GameObject eye3;
+    [SerializeField] private GameObject eye4;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        eye2.SetActive(false);
+        eye3.SetActive(false);
+        eye4.SetActive(false);
+    }
+}
