8d819ad7b17d4948725528d0663e61bf45256359
Stefan Klimpfinger <stefan.klimpfinger@gmx.at>
2020-04-20 12:05:46 +0200

    BackgroundMusic und VolumeSlider functionality added

---
+++ ludumdare46/Assets/Project/Scripts/BackgroundMusic.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusic : MonoBehaviour
+{
+    private AudioSource music;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+        music = GetComponent<AudioSource>();
+    }
+
+    public void PlayMusic()
+    {
+        if (music.isPlaying) return;
+        music.Play();
+    }
+
+    public void StopMusic()
+    {
+        music.Stop();
+    }
+}
